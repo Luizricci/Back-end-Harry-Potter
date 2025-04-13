c59d685bf19ea1994c31f68e292b6fb8844eb467
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-04-13 10:56:04 -0300

    refator: alter create wizard in controller

--- src/controllers/wizardController.js
+++ src/controllers/wizardController.js
@@ -23,8 +23,8 @@ const getWizard = async (req, res) => {
 
 const createWizard = async (req, res) => {
     try {
-        const { name, house_id } = req.body;
-        const newWizard = await wizardModel.createWizard(name, house_id);
+        const { name, house_id, image } = req.body;
+        const newWizard = await wizardModel.createWizard(name, house_id, image);
         res.status(201).json(newWizard);
     } catch (error) {
         res.status(500).json({ message: "Erro ao criar bruxo." });
